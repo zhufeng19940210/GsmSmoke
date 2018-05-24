fabddf807f4f7f6d8c2ef064b315e0e5506a33b1
zhufeng <358244144@qq.com>
2018-05-23 18:44:19 -0700

    创建项目提交下

---
+++ GSMSmoke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSMSmoke
+//
+//  Created by bailing on 2018/5/23.
+//  Copyright © 2018年 zhufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GSMSmoke/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GSMSmoke
+//
+//  Created by bailing on 2018/5/23.
+//  Copyright © 2018年 zhufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
